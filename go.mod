42667725a4e5bf0c0c4b463ed371f52aa36ebdcd
apurvmaurya <apurvmaurya5@gmail.com>
2023-12-16 01:24:00 +0530

    hands-on-exercise

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module pract
 
 go 1.21.5
-
-require github.com/gitApurv/dog v1.2.0
--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gitApurv/dog"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Println(dog.DogName)
-	fmt.Println(dog.Doggy)
+	x, y := rand.Intn(10), rand.Intn(10)
+	fmt.Printf("The value of x is %v\n", x)
+	fmt.Printf("The value of y is %v\n", y)
+
+	switch {
+	case x < 4 && y < 4:
+		fmt.Println("x and y are both less than 4")
+	case x > 6 && y > 6:
+		fmt.Println("x and y are both greater than 6")
+	case x >= 4 && x <= 6:
+		fmt.Println("x is greater than or equal to 4 and less than or equal to 6")
+	case y != 5:
+		fmt.Println("y is not 5")
+	default:
+		fmt.Println("none of the previous cases were meant")
+	}
 }
